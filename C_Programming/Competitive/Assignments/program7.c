243b745ef4a461ac2de4519382c25e83f97f84d9
Dikshahaah-27 <dikshadhage2005@gmail.com>
2026-06-20 19:40:36 +0530

    Added some competitive programs

---
+++ C_Programming/Competitive/Assignments/program7.c
@@ -0,0 +1,28 @@
+// Accept one number from user and print that number of * on the screen
+
+#include<stdio.h>
+
+void Display(int iNo)
+{
+    int iCnt = 0;
+
+    iCnt = 1;
+
+    while(iNo >= iCnt)
+    {
+        printf("*\t");
+        iNo--;
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter number :\n");
+    scanf("%d",&iValue);
+
+    Display(iValue);
+
+    return 0;
+}
